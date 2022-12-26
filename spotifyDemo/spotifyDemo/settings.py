90110a87a8a6b6b9cfb375ddd678b2b965aee8e0
Princekumar Dobariya <princekumar@bbd.co.za>
2022-12-26 21:44:25 +0530

    changes to available hosts

--- spotifyDemo/spotifyDemo/settings.py
+++ spotifyDemo/spotifyDemo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-br6*)e(@g(pq_4%z5(%6z3g4t1&245_m*lpd@88r19how2f#g+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
