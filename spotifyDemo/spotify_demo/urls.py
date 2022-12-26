39e72d9417cc4e8cc5d38cbf4356fed23aebb931
Princekumar Dobariya <princekumar@bbd.co.za>
2022-12-26 21:57:13 +0530

    csrfexepmt

--- spotifyDemo/spotify_demo/urls.py
+++ spotifyDemo/spotify_demo/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('spotify/', views.spotifyView.as_view(),name='spotify'),
+    path('spotify/', csrf_exempt(views.spotifyView.as_view()),name='spotify'),
 ]
